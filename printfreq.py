fb2b7b18e95a9f25be919d2d05042170d23d9536
Martin Almaraz <martin.almaraz@gmail.com>
2015-03-04 15:27:49 -0800

    readded printfreq.py

---
+++ printfreq.py
@@ -0,0 +1,38 @@
+import wave
+import struct
+import numpy
+from scipy import signal
+
+
+def open_file():
+	wav = wave.open("test2.wav", 'r') # opens and reads the wav file
+
+	(nchannels, sampwidth, framerate, nframes, comptype, compname) = wav.getparams() # Gets the appropriate 											 #things
+
+	frames = wav.readframes(nframes * nchannels) # calcs the framerate
+
+	out = struct.unpack_from ("%dh" % nframes * nchannels, frames)
+	
+	left = out[0:2] # we will only be dealing with one channel
+			# dual channel is not nessesary since we are only
+			# reading notes
+	print nchannels
+	print sampwidth
+	print framerate
+	print nframes
+	return out
+
+def fftConvert(frames):
+	
+	autocorrelated = signal.fftconvolve(frames, frames[::-1], mode = 'full')
+	return autocorrelated
+
+def getFreq(autocorrelated):
+	
+
+def main():
+	frames = open_file()
+	autocorrelated = fftConvert(frames)
+	print autocorrelated
+
+main()
